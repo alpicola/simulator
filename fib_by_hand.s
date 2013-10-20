85ded9a5a52c832b77de418f26e8bb13f91eb366
alpicola <ryo@alpico.la>
2013-10-20 22:08:42 +0900

    rename fib -> fib_by_hand

--- fib_by_hand.s
+++ fib_by_hand.s
@@ -1,11 +1,12 @@
 	j	main
 fib:
-	addi	r4, r4, -1
-	blez	r4, le
+	addi	r8, r4, -1
+	blez	r8, le
 	addi	r29, r29, -24
 	sw	r18, 16(r29)
 	sw	r31, 20(r29)
-	move	r18, r4
+	move	r4, r8
+	move	r18, r8
 	jal	fib
 	move	r8, r2
 	addi	r4, r18, -1
@@ -17,12 +18,11 @@ fib:
 	addi	r29, r29, 24
 	jr	r31
 le:
-	lli	r2, 1
+	move	r2, r4
 	jr	r31
 main:
 	addi	r29, r0, 1
 	sll	r29, r29, 20
-	move	r30, r29
 	lli	r4, 30
 	jal	fib
 	ow	r2
